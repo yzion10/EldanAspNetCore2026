6dd65f75d13787e1fb27774f2dd06f0c5a111270
yosizion <yosi.zi@eldan.co.il>
2026-04-23 09:43:13 +0300

    modify ApiLesson2

--- WebApiLessons/ApiLesson2/DataStores/CitiesDataStore.cs
+++ WebApiLessons/ApiLesson2/DataStores/CitiesDataStore.cs
@@ -10,8 +10,17 @@ namespace ApiLesson2.DataStores
         {
             Current = new List<CityDTO>()
             {
-                new CityDTO{ID = 1, Name = "Tel-Aviv"},
-                new CityDTO{ID = 2, Name = "Jerusalem"},
+                new CityDTO{ID = 1, Name = "Tel-Aviv", LandMarks = new List<LandMarkDTO>
+                {
+                    new LandMarkDTO{Id = 1, Name = "Azrieli", Description = "A shopping mall"},
+                    new LandMarkDTO{Id = 2, Name = "Rothschild", Description = "A street"}
+                }
+                },
+                new CityDTO{ID = 2, Name = "Jerusalem", LandMarks = new List<LandMarkDTO>
+                {
+                    new LandMarkDTO{Id = 3, Name = "Western Wall", Description = "A religious site"},
+                    new LandMarkDTO{Id = 4, Name = "Dome of the Rock", Description = "An Islamic shrine"}
+                }},
             };
         }
     }
