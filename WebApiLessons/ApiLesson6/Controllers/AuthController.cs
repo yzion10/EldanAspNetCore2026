d77d4d280840b55d42e9fb863ce29c6b1d9a2b6c
yosizion <yosi.zi@eldan.co.il>
2026-05-28 15:22:14 +0300

    AuthController

---
+++ WebApiLessons/ApiLesson6/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using ApiLesson6.DTO;
+using ApiLesson6.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLesson6.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly List<User> _users = new List<User>
+        {
+            new User { FirstName = "Admin", LastName = "User", UserName = "admin", Password = "admin" },
+            new User { FirstName = "yosi", LastName = "Doe", UserName = "yosi", Password = "1234" }
+        };
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(LoginBodyDTO loginBody)
+        {
+            var user = ValidateUser(loginBody.UserName, loginBody.Password);
+
+            if (user != null)
+                return Ok(user);
+            else
+                return Unauthorized();
+        }
+
+        private User ValidateUser(string userName, string password)
+        {
+            return _users.FirstOrDefault(u => u.UserName == userName && u.Password == password);
+        }
+    }
+}
---
+++ WebApiLessons/ApiLesson6/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiLesson6.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
